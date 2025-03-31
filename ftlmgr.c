82ebea513c212417b3f3b8dab97948e2409cbfb1
Jiwoong <jwchoi179@soongsil.ac.kr>
2025-03-31 09:32:07 +0900

    feat: 1번 조건 설계 완료

--- ftlmgr.c
+++ ftlmgr.c
@@ -4,7 +4,7 @@
 #include "flash.h"
 // 필요한 경우 헤더파일을 추가한다
 
-FILE *flashmemoryfp;	// fdevicedriver.c에서 사용
+FILE *flashmemoryfp; // fdevicedriver.c에서 사용
 
 //
 // 이 함수는 FTL의 역할 중 일부분을 수행하는데 물리적인 저장장치 flash memory에 Flash device driver를 이용하여 데이터를
@@ -15,12 +15,11 @@ FILE *flashmemoryfp;	// fdevicedriver.c에서 사용
 // 갱신은 강의자료의 in-place update를 따른다고 가정한다.
 // 스페어영역에 저장되는 정수는 하나이며, 반드시 binary integer 모드로 저장해야 한다.
 //
-int main(int argc, char *argv[])
-{	
+int main(int argc, char *argv[]) {
 	char sectorbuf[SECTOR_SIZE];
 	char pagebuf[PAGE_SIZE];
 	char *blockbuf;
-	
+
 	// flash memory 파일 생성: 위에서 선언한 flashmemoryfp를 사용하여 플래시 메모리 파일을 생성한다. 그 이유는 fdevicedriver.c에서 
 	//                 flashmemoryfp 파일포인터를 extern으로 선언하여 사용하기 때문이다.
 	// 페이지 쓰기: pagebuf의 섹터와 스페어에 각각 입력된 데이터를 정확히 저장하고 난 후 해당 인터페이스를 호출한다
@@ -28,5 +27,52 @@ int main(int argc, char *argv[])
 	//                  스페어 데이터를 분리해 낸다
 	// memset(), memcpy() 등의 함수를 이용하면 편리하다. 물론, 다른 방법으로 해결해도 무방하다.
 
-	return 0;
+	if (argc != 4) {
+		printf("사용법: %s c <flashfile> <#blocks>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	if (argv[1][0] != 'c') {
+		printf("잘못된 명령어입니다. 플래시 메모리를 생성하기 위해서는 옵션 'c'를 사용하세요.\n");
+		return EXIT_FAILURE;
+	}
+
+	char *flashfile = argv[2];
+	const int num_blocks = atol(argv[3]);
+
+	if (num_blocks <= 0) {
+		printf("블록의 수는 0보다 작으면 안됩니다.\n");
+		return EXIT_FAILURE;
+	}
+
+	// Flash memory 파일 생성
+	flashmemoryfp = fopen(flashfile, "wb");
+	if (flashmemoryfp == NULL) {
+		printf("Failed to create flash memory file.\n");
+		return EXIT_FAILURE;
+	}
+
+	// 각 블록을 0xFF로 초기화
+	blockbuf = (char *) malloc(num_blocks * BLOCK_SIZE);
+	if (blockbuf == NULL) {
+		printf("블록퍼퍼 생성에 실패했습니다.\n");
+		fclose(flashmemoryfp);
+		return EXIT_FAILURE;
+	}
+
+	memset(blockbuf, 0xFF, num_blocks * BLOCK_SIZE);
+
+	// 블록 수만큼 파일에 쓰기
+	for (int i = 0; i < num_blocks; i++) {
+		if (fwrite(blockbuf, BLOCK_SIZE, 1, flashmemoryfp) != 1) {
+			printf("%d 블록에 초기화 실패했습니다.\n", i);
+			free(blockbuf);
+			fclose(flashmemoryfp);
+			return EXIT_FAILURE;
+		}
+	}
+
+	free(blockbuf);
+	fclose(flashmemoryfp);
+	return EXIT_SUCCESS;
 }
